c794bfd3f193f402c2da448906852b1d0d4473d6
mickeey2525 <m.tachibana25@gmail.com>
2022-05-16 17:47:49 +0900

    add schedules.go

--- schedules.go
+++ schedules.go
@@ -1,7 +1,10 @@
 package digdaggo
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"time"
 )
@@ -73,6 +76,130 @@ func (c *Client) GetScheduleWithId(ctx context.Context, scheduleId int) (*Schedu
 
 func (c *Client) DisableScheduleWithId(ctx context.Context, scheduleId int) (*Schedule, error) {
 	req, err := c.newRequest(ctx, "POST", fmt.Sprintf("schedules/%d/disable", scheduleId), nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	checkError := c.checkHttpResponseCode(resp)
+	if checkError != nil {
+		return nil, checkError
+	}
+	var schedule Schedule
+	err = c.decodeBody(resp, &schedule)
+	if err != nil {
+		return nil, err
+	}
+	return &schedule, nil
+}
+
+type backFill struct {
+	fromTime    int64
+	attemptName string
+	dryRun      bool
+	count       int32
+}
+
+func newBackFill(fromTime int64, attemptName string, dryRun bool, count int32) (*backFill, error) {
+	if attemptName != "" {
+		return nil, errors.New("attempt name must not be empty")
+	}
+	return &backFill{
+		fromTime:    fromTime,
+		attemptName: attemptName,
+		dryRun:      dryRun,
+		count:       count,
+	}, nil
+}
+
+func (c *Client) BackfillSchedule(ctx context.Context, scheduleId, fromTime int64, attemptName string, dryRun bool, count int32) (*Schedule, error) {
+	bf, err := newBackFill(fromTime, attemptName, dryRun, count)
+	if err != nil {
+		return nil, err
+	}
+	bd, err := json.Marshal(bf)
+	if err != nil {
+		return nil, err
+	}
+	req, err := c.newRequest(ctx, "POST", fmt.Sprintf("schedules/%d/disable", scheduleId), nil, bytes.NewBuffer(bd))
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	checkError := c.checkHttpResponseCode(resp)
+	if checkError != nil {
+		return nil, checkError
+	}
+	var schedule Schedule
+	err = c.decodeBody(resp, &schedule)
+	if err != nil {
+		return nil, err
+	}
+	return &schedule, nil
+}
+
+func (c *Client) EnableSchedule(ctx context.Context, scheduleId int, skipSchedule bool, nextTime string) (*Schedule, error) {
+	if nextTime != "" {
+		return nil, errors.New("nextTime must not be empty")
+	}
+	nextSched := struct {
+		skipSchedule bool
+		nextTime     string
+	}{
+		skipSchedule: skipSchedule,
+		nextTime:     nextTime,
+	}
+	bd, err := json.Marshal(nextSched)
+	if err != nil {
+		return nil, err
+	}
+	req, err := c.newRequest(ctx, "POST", fmt.Sprintf("schedules/%d/disable", scheduleId), nil, bytes.NewBuffer(bd))
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	checkError := c.checkHttpResponseCode(resp)
+	if checkError != nil {
+		return nil, checkError
+	}
+	var schedule Schedule
+	err = c.decodeBody(resp, &schedule)
+	if err != nil {
+		return nil, err
+	}
+	return &schedule, nil
+}
+
+func (c *Client) SkipSchedule(ctx context.Context, scheduleId, nextRunTime, fromTime int64, dryRun bool, nextTime string, count int32) (*Schedule, error) {
+	if nextTime != "" {
+		return nil, errors.New("nextTime must not be empty")
+	}
+	skipSched := struct {
+		nextRuntime int64
+		nextTime    string
+		fromTime    int64
+		dryRun      bool
+		count       int32
+	}{
+		nextRuntime: nextRunTime,
+		nextTime:    nextTime,
+		fromTime:    fromTime,
+		dryRun:      dryRun,
+		count:       count,
+	}
+	bd, err := json.Marshal(skipSched)
+	if err != nil {
+		return nil, err
+	}
+	req, err := c.newRequest(ctx, "POST", fmt.Sprintf("schedules/%d/disable", scheduleId), nil, bytes.NewBuffer(bd))
 	if err != nil {
 		return nil, err
 	}
